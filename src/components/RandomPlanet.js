4d7fb6739fd0b9895b2c1b2dd441eb4ec5f53c85
gauss <maesh.kyryll@gmail.com>
2019-09-29 19:43:16 +0300

    I don't know why. Just move on.

--- src/components/RandomPlanet.js
+++ src/components/RandomPlanet.js
@@ -2,78 +2,75 @@ import React, {Component, Fragment} from 'react';
 
 import './styles/css/random-planet.css';
 import SwapiService from "../services/SwapiService";
-import {ErrorIndicator, Spinner} from "./index";
+import { ErrorBoundary, Spinner } from "./index";
+import ImageService from "../services/ImageService";
 
 export default class RandomPlanet extends Component {
+
+    imageService = new ImageService();
 
     constructor(props) {
         super(props);
         this.state = {
-            planet: {
-                id: (Math.floor(Math.random() * 25) + 2)
-            },
+            id: (Math.floor(Math.random() * 25) + 2),
+            planet: null,
             loading: true,
-            error: false
         };
-    };
-
-    onRandomPlanetLoaded = (planet) => {
-        this.setState(({planet: {id}}) => ({
-            planet: {id, ...planet},
-            loading: false
-        }));
     };
 
     fetchRandomPlanet () {
         const swapiService = new SwapiService();
-        swapiService.getPlanet(this.state.planet.id)
-            .then(this.onRandomPlanetLoaded)
-            .catch(this.onError);
+        swapiService.getPlanet(this.state.id)
+            .then((planet) => {
+                this.setState({planet, loading: false});
+            });
     };
 
     componentDidMount() {
         this.fetchRandomPlanet();
     }
 
-    onError = (error) =>  {
-        this.setState({error: true, loading: false});
-    };
-
     render() {
+
+        if (!this.state.planet) {
+            return null;
+        }
+
         const {
             loading,
-            error,
             planet: {id, name, population, rotationPeriod, diameter}
         } = this.state;
 
         return (
-            <div className="random-planet jumbotron rounded">
-                {error ? (
-                    <ErrorIndicator/>
-                ) : (
-                    loading ? <Spinner/>
-                    : <Fragment>
-                            <img className="planet-image" src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
-                            <div>
-                                <h4>{name}</h4>
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">
-                                        <span className="term">Population</span>
-                                        <span>{population}</span>
-                                    </li>
-                                    <li className="list-group-item">
-                                        <span className="term">Rotation Period</span>
-                                        <span>{rotationPeriod}</span>
-                                    </li>
-                                    <li className="list-group-item">
-                                        <span className="term">Diameter</span>
-                                        <span>{diameter}</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </Fragment>
-                )}
-            </div>
+            <ErrorBoundary>
+                <div className="random-planet jumbotron rounded">
+                    { loading
+                        ? <Spinner/>
+                        : (
+                            <Fragment>
+                                <img className="planet-image" src={this.imageService.getPlanetImage(id)}/>
+                                <div>
+                                    <h4>{name}</h4>
+                                    <ul className="list-group list-group-flush">
+                                        <li className="list-group-item">
+                                            <span className="term">Population</span>
+                                            <span>{population}</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <span className="term">Rotation Period</span>
+                                            <span>{rotationPeriod}</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <span className="term">Diameter</span>
+                                            <span>{diameter}</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Fragment>
+                        )
+                    }
+                </div>
+            </ErrorBoundary>
         );
     }
 }
